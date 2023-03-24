167141197895142799b784c55f732abcfcc793d4
Tanzim <mdtanzimehsan@gmail.com>
2023-03-25 03:35:58 +0600

    Practice 03

---
+++ 12_practice03.js
@@ -0,0 +1,29 @@
+//problem 1
+/*let marks ={
+   tanzim:10,
+   ehsan:15,
+zihad:20
+}
+for (let i=0;i<Object.keys(marks).length;i++)
+{
+  console.log(Object.keys(marks)[i]+ " " +marks[Object.keys(marks)[i]])
+ }
+*/             
+
+//problem no 2
+/*for (let key in marks){
+  console.log(key+marks[key])
+}
+*/
+//Problem no 3 
+/*let correct_no = 61
+let i
+while(i!=correct_no){
+  i=prompt("Enter a no")
+}
+*/
+//Problem no 4
+const mean = (a,b,c,d) => {
+    return (a + b + c + d) / 4
+  }
+  console.log(mean(4,5,6,7))
