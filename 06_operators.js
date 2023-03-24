f55a974d237b01d7d5e692373a8e1956e1f27670
Tanzim <mdtanzimehsan@gmail.com>
2023-03-25 03:28:56 +0600

    Operators

---
+++ 06_operators.js
@@ -0,0 +1,27 @@
+console.log("Operators in js")
+//Arithmetic operator
+let a = 45;
+let b = 7;
+console.log("a+b= ",a+b)
+console.log("a-b= ",a-b)
+console.log("a/b= ",a/b)
+console.log("a*b= ",a*b)
+console.log("a%b= ",a%b) 
+console.log("a**b= ",a**b) 
+console.log("a**b= ",a**b) 
+console.log("a**b= ",a**b) 
+//Incremet oprator
+console.log("a =",a++)
+console.log(a)
+//console.log("b =",b--)
+//console.log(b)
+console.log("b =",--b)
+console.log(b) 
+
+// comparison Operator
+let comp1 = 6;
+let comp2 = "6";
+console.log("comp1== comp2 is ",comp1 ==comp2)
+console.log("comp1!= comp2 is ",comp1 !=comp2)
+console.log("comp1=== comp2 is ",comp1 === comp2)
+console.log("comp1 !== comp2 is ",comp1!==comp2)
