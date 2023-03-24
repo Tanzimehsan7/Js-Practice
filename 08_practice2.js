2d4c83d98447918662042d583e84a40cb635dd6c
Tanzim <mdtanzimehsan@gmail.com>
2023-03-25 03:30:48 +0600

    Practice 02

---
+++ 08_practice2.js
@@ -0,0 +1,46 @@
+
+/*let age =prompt("what is is your age")
+if(age>10 && age <20){
+  console.log("Your age between 20")
+}
+   else{
+ console.log ("more than 20")
+}
+*/
+/*
+let age =prompt("what is is your age")
+switch (age){
+  case '12':console.log("your age is 12")
+    break
+  case '13':console.log("your age is 13")
+    break
+  case '14':console.log("your age is 14")
+    break
+  case '15':console.log("your age is 15")
+    break
+  default:
+    console.log("your age is not special")
+}
+*/
+/*let n = prompt("What is the number?")
+n=Number.parseInt(n)
+if(n%2 == 0 &&  n%3 == 0 ){
+  console.log("num is disivle by 2 and 3")
+  }
+else{
+  console.log("num is not visible 2 and 3")
+}
+*/
+
+/*let n = prompt("What is the number?")
+n=Number.parseInt(n)
+if(n%2 == 0 ||  n%3 == 0 ){
+  console.log("num is disivle by 2 and 3")
+  }
+else{
+  console.log("num is not visible 2 and 3")
+}
+*/
+let age = 20
+let a = age >18 ? "you can drive": "you can not drive"
+console.log(a)
