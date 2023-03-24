3b11801e6d31abbfd1db0be89f4a41114cd353d8
Tanzim <mdtanzimehsan@gmail.com>
2023-03-25 03:25:01 +0600

    let_const_var

---
+++ 03_var_let_const.js
@@ -0,0 +1,11 @@
+console.log("Javascript:var,let,const")
+//var a = 12;
+let b = "tanzim";
+let c = null;
+let d =undefined
+{let b = 'my name is'
+console.log(b)
+}
+console.log(b)
+const author ="Tanzim"
+console.log(author)
