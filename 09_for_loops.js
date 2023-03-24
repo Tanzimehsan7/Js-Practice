68ea7d6371708826318a561cbf85437bf87b7396
Tanzim <mdtanzimehsan@gmail.com>
2023-03-25 03:31:49 +0600

    For loops

---
+++ 09_for_loops.js
@@ -0,0 +1,26 @@
+//program to first natural number
+/*let sum = 0
+let n = prompt("enter value")
+n=Number.parseInt(n)
+for (let i =0; i < n;i++){
+  sum  += (i+1)
+  console.log((i+1),"+")
+}
+console.log(sum)
+*/
+
+//FOR IN LOOPS
+/*let object ={
+  tanzim : 10,
+  ehsan:   21,
+   zihad: 20 
+}
+for (a in object){
+  console.log(a,object[a])
+}
+*/
+
+//For of loops
+for (b of  "Tanzim"){
+    console.log(b)
+  }
