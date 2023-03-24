073e2e290198ca8edfd4efb8cabd97853257f715
Tanzim <mdtanzimehsan@gmail.com>
2023-03-25 03:34:44 +0600

    Function

---
+++ 11_Function.js
@@ -0,0 +1,28 @@
+/*function cal (x,y){
+  return Math.round(1+(x+y)/2)
+}
+
+let a =1;
+let b =2;
+let c =3;
+console.log(cal(a,b))
+console.log(cal(a,c))
+console.log(cal(b,c))
+*/
+
+/*function cal (x,y,z){
+  return ((x+y+z)/2)
+}
+
+let a =1;
+let b =2;
+let c =3;
+console.log(cal(a,b,c))
+*/
+function hello() {
+    console.log("my name is tanzim");
+    return "hi";
+  
+  }
+  let v = hello();
+  console.log(v)
