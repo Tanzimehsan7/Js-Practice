bc16c5e232c09468c7938a8a6a013cf3cbb1c044
Tanzim <mdtanzimehsan@gmail.com>
2023-03-25 03:32:43 +0600

    While loops

---
+++ 10_while_loops.js
@@ -0,0 +1,15 @@
+/*let n = prompt("Enter the value of n")
+n=Number.parseInt(n)
+let i = 0
+while(i<n){
+  console.log(i)
+  i++;
+}
+*/
+let n = prompt("Enter the value of n")
+n=Number.parseInt(n)
+let i = 10
+do{
+  console.log(i)
+  i++;
+}while(i<n)
