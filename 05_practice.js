7bdca9133b925864336aba6212c0ad33ea05ba57
Tanzim <mdtanzimehsan@gmail.com>
2023-03-25 03:27:48 +0600

    Practice 1

---
+++ 05_practice.js
@@ -0,0 +1,32 @@
+//cahpter 1 Q1:
+//create a variable of type string and try to add a number to it.
+let a = "tanzim"
+let b =6
+console.log(a+b)
+
+//cahpter 1 Q2:
+//use typeof oprator to find the datatype of the string in lass question
+console.log(typeof (a+b))
+
+//cahpter 1 Q3:
+//Creat a const object in javaScript can you change it to hold a number
+const d  ={
+  name:"tanzim",
+  section:1,
+  ispriciple:false
+  
+}
+//cahpter 1 Q4:
+//try to add a new key to the const object i problem 3 were you able to do it
+d['friend']="tanzim"
+d['name']="Tanzim Ehsan"
+console.log(d)
+
+//cahpter 1 Q5:
+//write a js program to creat a word-meaning dictionary of 3 words
+const dic ={
+  tenacious:"adjective holding together",
+  add:"sum",
+  love:"life"
+}
+console.log(dic.tenacious)
