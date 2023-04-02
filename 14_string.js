40dde78bd74c1c8622ffce26361808260a3e80bf
Tanzim <mdtanzimehsan@gmail.com>
2023-04-02 09:08:21 +0600

    string

---
+++ 14_string.js
@@ -0,0 +1,15 @@
+
+let name = "TANZIM"
+console.log(name.length)
+let friend = 'borshon'
+console.log(friend.length)
+let boy1 ='tanzim'
+let boy2 ="ehsan"
+let sentence = `my name is tanzim ehsan`
+console.log(sentence)
+
+//Escape sequence Characters
+let fruit ='Bana\'na'
+console.log(fruit)
+let fruit1 ='Bana\nna'
+console.log(fruit1)
