7d4f7b02aa8a5a6d4cf166cea47becaa98bf1ad2
Tanzim <mdtanzimehsan@gmail.com>
2023-04-02 09:51:02 +0600

    exercise

---
+++ 16_exercise.js
@@ -0,0 +1,13 @@
+//What will the following print in javascript?
+console.log("har\"".length)
+// ans:-4
+
+//write a progaram to convert a given string ti uppercase
+let name ="Tanzim Ehsan"
+console.log(name.toUpperCase()) 
+
+//Extract the amount out of this string
+let str ="please give tk 1000"
+console.log(str.slice(15))
+//console.log(str.slice("please give tk ".length))
+console.log(str.replace("give","some"))
