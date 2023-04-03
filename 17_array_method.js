ea97f93d63bbc40b2c76cdfc8eb4483782de7c48
Tanzim <mdtanzimehsan@gmail.com>
2023-04-04 05:28:16 +0600

    Araay methods

---
+++ 17_array_method.js
@@ -0,0 +1,31 @@
+//Array Method
+//toString
+
+/*let num =[1,2,3,4]
+let c =num.toString()
+console.log(c,typeof c)
+//join()
+let b = num.join(" ans ")
+console.log(b,typeof b)
+//shift()
+//let a =num.shift()
+//console.log(a)
+//console.log(num)
+//delete()
+delete num[0]
+console.log(num)
+console.log(num.length)
+*/
+//SHORT()
+/*let num=[5,3,6,8,4,6]
+num.sort()
+console.log
+*/
+
+//assending method()
+let compare=(a,b)=>{
+    return a-b
+}
+let num=[5,3,6,8,40,60]
+num.sort(compare)
+console.log(num)
