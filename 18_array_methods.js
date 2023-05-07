6e0246fd0fc8e0ca551fea685c64fc1863026dbc
Tanzim <mdtanzimehsan@gmail.com>
2023-05-07 06:30:15 +0600

    Array Methods

--- 18_array_methods.js
+++ 18_array_methods.js
@@ -1,4 +1,11 @@
-let name1 ="Tanzim"
+/*let name1 ="Tanzim"
 let namenew =Array.from(name1)
 console.log(namenew[3])
 console.log(typeof(namenew))
+*/
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map(function(item){
+    return item * 2
+});
+console.log(doubled); // [2, 4, 6, 8, 10]
+
